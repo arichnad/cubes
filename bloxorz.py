16dccb9a702a3ee32f5dad470b58113136c9b853
adrian <adrian-sw@aporter.org>
2018-10-30 16:50:19 -0400

    minor:  code improvements.

--- bloxorz.py
+++ bloxorz.py
@@ -13,33 +13,31 @@ board=[
 #board[-1]=board[-1][:-1]+'T'
 
 for rowNumber, row in enumerate(board):
-	if row.find('T') != -1: endPosition = (row.find('T'), rowNumber, 0, 0, 1)
-	if row.find('S') != -1: startPosition = (row.find('S'), rowNumber, 0, 0, 1)
+	if row.find('S')!=-1: startPosition = (row.find('S'), rowNumber, 0, 0)
+	if row.find('T')!=-1: endPosition = (row.find('T'), rowNumber, 0, 0)
 
 #breadth first search, please
 
 queue=[(startPosition, [])]
 seen=set()
 
-def append(queue, seen, newState, moves, newMove):
-	if newState in seen: return
-	x, y, dx, dy, dz = newState
-	if x<0 or y<0 or x+dx>=len(board[0]) or y+dy>=len(board): return
-	if board[y][x]=='-' or board[y+dy][x+dx] == '-': return
+def append(newState, moves, newMove):
+	x, y, dx, dy = newState
+	if newState in seen or not 0<=x<len(board[0])-dx or not 0<=y<len(board)-dy or board[y][x]=='-' or board[y+dy][x+dx] == '-': return
 	#the reason we wait to add to moves until here is for performance reasons:
 	moves = moves + [newMove]
+	seen.add(newState)
+	queue.append((newState, moves))
 	if newState == endPosition:
 		print(moves)
 		return True
-	seen.add(newState)
-	queue.append((newState, moves))
 
 while len(queue) > 0:
-	(x, y, dx, dy, dz), moves = queue.pop(0)
+	(x, y, dx, dy), moves = queue.pop(0);dz=1-dx-dy
 
-	if append(queue, seen, (x+(dx*2+dy+dz), y, dz, dy, dx), moves, 'right') or \
-		append(queue, seen, (x-(dz*2+dy+dx), y, dz, dy, dx), moves, 'left') or \
-		append(queue, seen, (x, y+(dy*2+dx+dz), dx, dz, dy), moves, 'down') or \
-		append(queue, seen, (x, y-(dz*2+dy+dx), dx, dz, dy), moves, 'up'):
+	if append((x+dx+1, y, dz, dy), moves, 'right') or \
+		append((x-dz-1, y, dz, dy), moves, 'left') or \
+		append((x, y+dy+1, dx, dz), moves, 'down') or \
+		append((x, y-dz-1, dx, dz), moves, 'up'):
 		break
 
